117d6280213172112dedcac8126acea787b8f896
marinerougier <m.rougier@hotmail.fr>
2018-06-15 17:07:29 +0200

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -1,7 +1,8 @@
 // à editer : 
 var min_height = 900;
 var min_width  = 1600;
-var url        = "https://www.psytoolkit.org/cgi-bin/psy2.4.1/survey?s=kmdPt";
+var url_high        = "https://www.psytoolkit.org/cgi-bin/psy2.4.1/survey?s=5dMuv";
+var url_low        = "https://www.psytoolkit.org/cgi-bin/psy2.4.1/survey?s=Qj6kG";
 
 // script -------------------------------------------------------------------------------------------
 var is_safari = /^((?!chrome|android).)*safari+ie/i.test(navigator.userAgent);
@@ -10,22 +11,10 @@ var width = window.screen.availWidth;
 var is_compatible = (!is_safari && height >= min_height && width >= min_width );
 
 if(!is_compatible) {
-
-    var safari_exclusion = {
-        type: "html-keyboard-response",
-        stimulus:
-        "<p>Unfortunately, your computer is not compatible with this experiment.</p>" +
-        "<p>Among the possible reasons :</p>" +
-        "<ul><li>You are using Safari</li><li>Your screen resolution is too low (lower than 1600 x 900) </li></ul>",
-        choices: jsPsych.NO_KEYS
+  window.location.href = url_low;
     };
 
-    var timeline_safari = [];
-
-    timeline_safari.push(safari_exclusion);
-    jsPsych.init({timeline: timeline_safari});
-
-} else {
-  window.location.href = url;
+else {
+  window.location.href = url_high;
 }
 
